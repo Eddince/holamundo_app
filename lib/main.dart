8fe80e2b7f5395facbb9252304b9552b33ac1579
Eddince <126356013+Eddince@users.noreply.github.com>
2025-04-22 09:59:14 -0400

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
-// Simulación de una app Flutter sin SDK real
+
+//void main() {
+  //print("¡Hola Mundo desde Dart!"); 
+  
+//}
+
+import 'package:flutter/material.dart';
+
 void main() {
-  print("¡Hola Mundo desde Dart!"); // Esto solo se verá en consola.
-  // En un entorno real, aquí iría el código de Flutter.
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('¡Hola Mundo!', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    ),
+  );
 }
